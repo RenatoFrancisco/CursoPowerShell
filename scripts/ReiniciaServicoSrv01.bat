853d7acce0c5b7581b2b11ae6fb3777d87b440ff
renatofrancisco <renatofrancisco.s@gmail.com>
2021-05-23 12:05:39 -0300

    Tentando solucionar com o antigo CMD

---
+++ scripts/ReiniciaServicoSrv01.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+echo Buscando Servico "ByteBanck Interface Service" no servidor SRV01
+sc \\SRV01 query "ByteBack Interface Service" | find /i "state" | find /i "running"
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Ops, servico nao sendo executado no servidor!
+) ELSE (
+    echo Parando servico...
+    sc \\ SRV stop "ByteBack Interface Service" > NULL
+    echo Iniciando servico...
+    sc \\ SRV01 start "ByteBank Interface Service" > NUL
+    echo Servico Reiniciado com sucesso!
+    
